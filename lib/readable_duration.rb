219cf12dc4ea07e3e7d06bdc6ffd7a8c988b5a45
eldenhowes <williameldenhowes@gmail.com>
2019-09-22 18:35:19 +0100

    refactored further to use one method for handling all printing of units, instead of one for each unit_type

--- lib/readable_duration.rb
+++ lib/readable_duration.rb
@@ -1,38 +1,18 @@
 def readable_duration(seconds)
-  array_for_printing = [print_hours(seconds / 3600),
-                        print_minutes(seconds % 3600 / 60),
-                        print_seconds(seconds % 3600 % 60 % 60)]
+  array_for_printing = [print_unit(seconds / 3600, "hour"),
+                        print_unit(seconds % 3600 / 60, "minute"),
+                        print_unit(seconds % 3600 % 60 % 60, "second")]
   array_for_printing.reject!(&:empty?)
   return handle_printing(array_for_printing)
 end
 
 private
 
-def print_seconds(no_of_seconds)
-  if no_of_seconds == 1
-    "#{no_of_seconds} second"
-  elsif no_of_seconds > 1
-    "#{no_of_seconds} seconds"
-  else
-    ""
-  end
-end
-
-def print_minutes(no_of_minutes)
-  if no_of_minutes == 1
-    "#{no_of_minutes} minute"
-  elsif no_of_minutes > 1
-    "#{no_of_minutes} minutes"
-  else
-    ""
-  end
-end
-
-def print_hours(no_of_hours)
-  if no_of_hours == 1
-    "#{no_of_hours} hour"
-  elsif no_of_hours > 1
-    "#{no_of_hours} hours"
+def print_unit(no_of_units, unit_type)
+  if no_of_units == 1
+    "#{no_of_units} #{unit_type}"
+  elsif no_of_units > 1
+    "#{no_of_units} #{unit_type}s"
   else
     ""
   end
