610a7f12f735b983da1bd2939e9e56e7e7fb18bf
eldenhowes <williameldenhowes@gmail.com>
2019-09-22 14:53:50 +0100

    Refactored to include a private method for handling hours separately

--- lib/readable_duration.rb
+++ lib/readable_duration.rb
@@ -5,8 +5,7 @@ def readable_duration(seconds)
 
   return handle_seconds(remaining_seconds) if seconds < 60
 
-  return "1 hour" if no_of_hours == 1
-  return "2 hours" if no_of_hours == 2
+  return handle_hours(no_of_hours) if seconds >= 3600
 
   if remaining_seconds > 0
     "#{handle_minutes(no_of_minutes)} and #{handle_seconds(remaining_seconds)}"
@@ -30,5 +29,13 @@ def handle_minutes(no_of_minutes)
     "#{no_of_minutes} minute"
   else
     "#{no_of_minutes} minutes"
+  end
+end
+
+def handle_hours(no_of_hours)
+  if no_of_hours == 1
+    "#{no_of_hours} hour"
+  else
+    "#{no_of_hours} hours"
   end
 end
