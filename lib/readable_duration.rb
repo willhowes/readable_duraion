5961cefaed9c009e645465e5a232ca5a69e57377
eldenhowes <williameldenhowes@gmail.com>
2019-09-22 14:34:25 +0100

    Refactored to bring in private methods to handle durations less than a minute and handle seconds formatting

--- lib/readable_duration.rb
+++ lib/readable_duration.rb
@@ -1,6 +1,19 @@
 def readable_duration(seconds)
-  return "#{seconds / 60} minute and #{seconds % 60} seconds" if seconds > 61
-  return "#{seconds / 60} minute and #{seconds % 60} second" if seconds == 61
-  return "#{seconds} second" if seconds == 1
-  "#{seconds} seconds"
+  remaining_seconds = seconds % 60
+  no_of_minutes = seconds / 60
+  
+  return handle_seconds(remaining_seconds) if less_than_minute?(seconds)
+  "#{no_of_minutes} minute and #{handle_seconds(remaining_seconds)}"
+end
+
+private
+
+def less_than_minute?(seconds)
+  return true if seconds < 60
+  false
+end
+
+def handle_seconds(remaining_seconds)
+  return "#{remaining_seconds} second" if remaining_seconds == 1
+  "#{remaining_seconds} seconds"
 end
